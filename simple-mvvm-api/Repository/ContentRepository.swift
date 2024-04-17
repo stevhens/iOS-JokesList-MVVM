7935b641eb9e8bb8af7cf21cf7197fb5896ba9fc
Stevhen <stevhen@snappymob.com>
2024-04-17 20:02:29 +0700

    Add Repository to fetch Joke detail

--- simple-mvvm-api/Repository/ContentRepository.swift
+++ simple-mvvm-api/Repository/ContentRepository.swift
@@ -2,4 +2,5 @@ import Foundation
 
 protocol ContentRepository {
     func fetchJokeList(completion: @escaping ([Joke]?, Error?) -> Void)
+    func fetchJoke(id: String, completion: @escaping (Joke?, Error?) -> Void)
 }
--- simple-mvvm-api/Repository/DefaultContentRepository.swift
+++ simple-mvvm-api/Repository/DefaultContentRepository.swift
@@ -14,4 +14,16 @@ class DefaultContentRepository: ContentRepository {
             }
         }
     }
+
+    func fetchJoke(id: String, completion: @escaping (Joke?, Error?) -> Void) {
+        let urlString = "https://official-joke-api.appspot.com/jokes/\(id)"
+        networkClient.fetchData(from: urlString) { (result: Result<Joke, Error>) in
+            switch result {
+            case .success(let response):
+                completion(response, nil)
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+    }
 }
