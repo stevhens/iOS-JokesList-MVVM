a0b9663aa3a73c6fc040f9111f8e1516ff3a95ba
Stevhen <stevhen@snappymob.com>
2024-04-17 20:15:19 +0700

    Implement JokeDetailViewModel

---
+++ simple-mvvm-api/Features/Joke/JokeDetailViewModel.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+class JokeDetailViewModel: ObservableObject {
+    @Published var joke: Joke?
+    @Published var isLoading: Bool = false
+    @Published var error: Error?
+    
+    private let contentRepository: ContentRepository
+    
+    init(contentRepository: ContentRepository = DefaultContentRepository()) {
+        self.contentRepository = contentRepository
+    }
+    
+    func fetchJoke(id: String) {
+        isLoading = true
+        error = nil
+        
+        contentRepository.fetchJoke(id: id) { [weak self] (joke, error) in
+            guard let self = self else { return }
+            self.isLoading = false
+            
+            if let error = error {
+                self.error = error
+                return
+            }
+            
+            if let joke = joke {
+                self.joke = joke
+            }
+        }
+    }
+}
